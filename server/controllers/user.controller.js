b201c453c3145e898584cc70764735f8b1990afc
seanarch <zhuqingyun@outlook.com>
2023-05-12 15:58:30 -0600

    Added some crud methods for users

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import { extend } from "lodash/extend";
 
 const create = async (req, res) => {
   const user = new User(req.body);
@@ -21,9 +22,48 @@ const list = async (req, res) => {
     return res.status(400).json({ error: err.message });
   }
 };
-const userByID = (req, res, next, id) => {};
-const read = (req, res) => {};
-const update = (req, res, next) => {};
-const remove = (req, res, next) => {};
+
+const userByID = async (req, res, next, id) => {
+  try {
+    let user = await User.findById(id);
+    if (!user) return res.status("400").json({ error: "User not found" });
+    req.profile = user;
+    next();
+  } catch (err) {
+    return res.status("400").json({ error: "Could not retrieve user" });
+  }
+};
+
+const read = (req, res) => {
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+  return res.json(req.profile);
+};
+
+const update = async (req, res, next) => {
+  try {
+    let user = req.profile;
+    user = extend(user, req.body);
+    user.updated = Date.now();
+    await user.save();
+    user.hashed_password = undefined;
+    user.salt = undefined;
+    res.json(user);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+};
+
+const remove = async (req, res, next) => {
+  try {
+    let user = req.profile;
+    let deletedUser = await user.remove();
+    deletedUser.hashed_password = undefined;
+    deletedUser.salt = undefined;
+    res.json(deletedUser);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+};
 
 export default { create, userByID, read, list, remove, update };

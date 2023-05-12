a025c5eee434cd38be40b44138956518956726ea
seanarch <zhuqingyun@outlook.com>
2023-05-11 23:13:44 -0600

    Config mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,20 @@
 import config from "../config/config.js";
 import app from "./express.js";
+import mongoose from "mongoose";
+import path from "path";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri);
+
+console.log(`Connected to mongodb ${process.env.MONGODB_URI}`);
 
 app.listen(config.port, (err) => {
   if (err) {

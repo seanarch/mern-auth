3044926db393850450978c6be44c6ce6c41d387f
seanarch <zhuqingyun@outlook.com>
2023-05-12 11:21:25 -0600

    Simple password validation in userschema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -55,4 +55,13 @@ UserSchema.methods = {
   },
 };
 
+UserSchema.path("hashed_password").validate(function (v) {
+  if (this._password && this._password.length < 6) {
+    this.invalidate("password", "Password must be at least 6 characters.");
+  }
+  if (this.isNew && !this._password) {
+    this.invalidate("password", "Password is required");
+  }
+}, null);
+
 export default mongoose.model("User", UserSchema);

25f2dd311510fe353ba1534ed9e55441a0aacee9
seanarch <zhuqingyun@outlook.com>
2023-05-12 23:54:40 -0600

    minor changes in auth and user route

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -8,10 +8,4 @@ router.route("/auth/signin").post(authCtrl.signin);
 
 router.route("/auth/signout").get(authCtrl.signout);
 
-router
-  .route("/api/users/:userId")
-  .get(authCtrl.requireSignin, userCtrl.read)
-  .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
-  .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove);
-
 export default router;
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userCtrl from "../controllers/user.controller.js";
+import authCtrl from "../controllers/auth.controller.js";
 
 // /api/users GET POST
 const router = express.Router();
@@ -9,9 +10,9 @@ router.route("/api/users").get(userCtrl.list).post(userCtrl.create);
 // /api/users/:userId GET one, PUT one, DELETE one
 router
   .route("/api/users/:userId")
-  .get(userCtrl.read)
-  .put(userCtrl.update)
-  .delete(userCtrl.remove);
+  .get(authCtrl.requireSignin, userCtrl.read)
+  .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
+  .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove);
 
 router.param("userId", userCtrl.userByID);
 

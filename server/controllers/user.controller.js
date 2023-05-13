0781f54028c92bde15861bd1ebae92956d8fb847
seanarch <zhuqingyun@outlook.com>
2023-05-12 23:46:56 -0600

    Minor changes

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -26,11 +26,11 @@ const list = async (req, res) => {
 const userByID = async (req, res, next, id) => {
   try {
     let user = await User.findById(id);
-    if (!user) return res.status("400").json({ error: "User not found" });
+    if (!user) return res.status(400).json({ error: "User not found" });
     req.profile = user;
     next();
   } catch (err) {
-    return res.status("400").json({ error: "Could not retrieve user" });
+    return res.status(400).json({ error: "Could not retrieve user" });
   }
 };
 
